b4f400b2e201a776ac87dc815f0bbc900b08dbd4
dev-rm <54745323+dev-rm@users.noreply.github.com>
2020-02-24 05:57:01 +0530

    Add Angular Screens

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { RegistrationService } from './services/registration.service';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [RegistrationService],
   bootstrap: [AppComponent]
--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -18,4 +18,13 @@ export class RegistrationService {
   getBikes() {
     return this.http.get('/server/api/v1/bikes');
   }
+
+  getBike(id: number){
+    return this.http.get('/server/api/v1/bikes/' +id);
+  }
+
+  createBikeRegistration(bike){
+    let body = JSON.stringify(bike);
+    return this.http.post('/server/api/v1/bikes',body, httpOptions);
+  }
 }
